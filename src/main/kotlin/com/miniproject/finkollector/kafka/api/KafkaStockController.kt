2d8b83c4f17562a48a80f9620e30b8a63ae0f83a
arxivj <xivpark1110@gmail.com>
2023-10-18 00:30:27 +0900

    [update] build

--- src/main/kotlin/com/miniproject/finkollector/kafka/api/KafkaStockController.kt
+++ src/main/kotlin/com/miniproject/finkollector/kafka/api/KafkaStockController.kt
@@ -1,52 +1,52 @@
-package com.miniproject.finkollector.kafka.api
-
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import org.springframework.core.io.ClassPathResource
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.io.File
-import java.time.format.DateTimeFormatter
-
-
-@RestController
-@RequestMapping("/kafka")
-class KafkaStockController() {
-
-    data class Stock(
-        val ticker: String,
-        val open: Double,
-        val high: Double,
-        val low: Double,
-        val close: Double,
-        val adjClose: Double,
-        val volume: Number,
-        val date: String
-    )
-
-    @GetMapping("/create-json") // 전체 json 데이터에서 날짜별 json 데이터를 생성해주는 메서드
-    fun createJson(){
-        val objectMapper = jacksonObjectMapper()
-
-        // 1. JSON 파일 읽기
-        val resource = ClassPathResource("/stock.json")
-        val stocks: List<Stock> = objectMapper.readValue(resource.inputStream)
-
-        // 2. 날짜별로 데이터 분류
-        val groupedByDate = stocks.groupBy { it.date }
-
-        // 3. 각 날짜별로 JSON 파일 생성
-        groupedByDate.forEach { (date, stocks) ->
-            val jsonContent = objectMapper.writeValueAsString(stocks)
-            val directory = File("stockdata")
-            if (!directory.exists()) {
-                directory.mkdir()
-            }
-            File("output-directory/${date.format(DateTimeFormatter.ISO_LOCAL_DATE)}.json").writeText(jsonContent)
-
-        }
-    }
-
-
-}
+//package com.miniproject.finkollector.kafka.api
+//
+//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+//import com.fasterxml.jackson.module.kotlin.readValue
+//import org.springframework.core.io.ClassPathResource
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//import java.io.File
+//import java.time.format.DateTimeFormatter
+//
+//
+//@RestController
+//@RequestMapping("/kafka")
+//class KafkaStockController() {
+//
+//    data class Stock(
+//        val ticker: String,
+//        val open: Double,
+//        val high: Double,
+//        val low: Double,
+//        val close: Double,
+//        val adjClose: Double,
+//        val volume: Number,
+//        val date: String
+//    )
+//
+//    @GetMapping("/create-json") // 전체 json 데이터에서 날짜별 json 데이터를 생성해주는 메서드
+//    fun createJson(){
+//        val objectMapper = jacksonObjectMapper()
+//
+//        // 1. JSON 파일 읽기
+//        val resource = ClassPathResource("/stock.json")
+//        val stocks: List<Stock> = objectMapper.readValue(resource.inputStream)
+//
+//        // 2. 날짜별로 데이터 분류
+//        val groupedByDate = stocks.groupBy { it.date }
+//
+//        // 3. 각 날짜별로 JSON 파일 생성
+//        groupedByDate.forEach { (date, stocks) ->
+//            val jsonContent = objectMapper.writeValueAsString(stocks)
+//            val directory = File("stockdata")
+//            if (!directory.exists()) {
+//                directory.mkdir()
+//            }
+//            File("output-directory/${date.format(DateTimeFormatter.ISO_LOCAL_DATE)}.json").writeText(jsonContent)
+//
+//        }
+//    }
+//
+//
+//}
--- src/main/kotlin/com/miniproject/finkollector/kafka/producer/KafkaProducerService.kt
+++ src/main/kotlin/com/miniproject/finkollector/kafka/producer/KafkaProducerService.kt
@@ -1,57 +1,57 @@
-package com.miniproject.finkollector.kafka.producer
-
-//import com.miniproject.finkollector.domain.kafka.ApiProperties
-//import com.miniproject.finkollector.domain.kafka.KafkaProperties
-import jakarta.annotation.PostConstruct
-import org.slf4j.LoggerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
-
-@Service // 외부 API에 주기적으로 시세 데이터를 요청 후 받아와서 topic 발행
-class KafkaProducerService(
-    private val restTemplate: RestTemplate,
-    var kafkaTemplate: KafkaTemplate<String, String>,
-//    val kafkaProperties: KafkaProperties,
-//    val apiProperties: ApiProperties
-) {
-        private val logger = LoggerFactory.getLogger(KafkaProducerService::class.java)
-//    @PostConstruct
-//    fun init() {
-//        fetchStockData()
-//    }
-
-    companion object {
-        private const val API_URL = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?your_parameters_here"
-    }
-
-//    @Scheduled(cron = "*/5 * * * * ?")
-//    fun fetchStockData() {
-//        try {
-//            val response: ResponseEntity<String> = restTemplate.getForEntity(API_URL, String::class.java)
-//            val stockData = response.body
+//package com.miniproject.finkollector.kafka.producer
 //
-//            // 위 stockData로 topic 발행
-//            stockData?.let {
-//                publishTopic(it)
-//            } ?: logger.warn("Received null stock data from API")
+////import com.miniproject.finkollector.domain.kafka.ApiProperties
+////import com.miniproject.finkollector.domain.kafka.KafkaProperties
+//import jakarta.annotation.PostConstruct
+//import org.slf4j.LoggerFactory
+//import org.springframework.kafka.core.KafkaTemplate
+//import org.springframework.stereotype.Service
+//import org.springframework.web.client.RestTemplate
+//import java.io.File
+//import java.text.SimpleDateFormat
+//import java.util.*
 //
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
+//@Service // 외부 API에 주기적으로 시세 데이터를 요청 후 받아와서 topic 발행
+//class KafkaProducerService(
+//    private val restTemplate: RestTemplate,
+//    var kafkaTemplate: KafkaTemplate<String, String>,
+////    val kafkaProperties: KafkaProperties,
+////    val apiProperties: ApiProperties
+//) {
+//        private val logger = LoggerFactory.getLogger(KafkaProducerService::class.java)
+////    @PostConstruct
+////    fun init() {
+////        fetchStockData()
+////    }
+//
+//    companion object {
+//        private const val API_URL = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?your_parameters_here"
 //    }
 //
-//    fun publishTopic(data: String) {
-//        kafkaTemplate.send("stock-list",data)
-//        logger.info("Data sent to topic $data")
-//    }
-
-
-
-
+////    @Scheduled(cron = "*/5 * * * * ?")
+////    fun fetchStockData() {
+////        try {
+////            val response: ResponseEntity<String> = restTemplate.getForEntity(API_URL, String::class.java)
+////            val stockData = response.body
+////
+////            // 위 stockData로 topic 발행
+////            stockData?.let {
+////                publishTopic(it)
+////            } ?: logger.warn("Received null stock data from API")
+////
+////        } catch (e: Exception) {
+////            e.printStackTrace()
+////        }
+////    }
+////
+////    fun publishTopic(data: String) {
+////        kafkaTemplate.send("stock-list",data)
+////        logger.info("Data sent to topic $data")
+////    }
+//
+//
+//
+//
 //    @PostConstruct //DB에 카프카 토픽 저장 코드
 //    fun sendAllFilesToKafka() {
 //        val startDate = SimpleDateFormat("yyyy-MM-dd").parse("2012-09-04")
@@ -83,7 +83,7 @@ class KafkaProducerService(
 //            currentDate = calendar.time
 //        }
 //    }
-
-
-
-}
+//
+//
+//
+//}
